5e08edcad77f4978890914a6f4450558d83a82df
jplucarova <192219926+jplucarova@users.noreply.github.com>
2025-03-31 17:44:42 +0200

    Add if __name__ == "__main__": and replace variable and function names
    if __name__ == "__main__": added before fc execution, gdp_df replace by
    k_df, fc name changed

---
+++ kurzy.py
@@ -0,0 +1,57 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from io import StringIO
+
+def get_tab(url):
+	try:
+		# send a GET request to the URL
+		response = requests.get(url)
+		response.raise_for_status()  # check if the request was successful
+	except requests.exceptions.RequestException as err:
+		print(f"Error fetching the webpage: {err}")
+		return None
+	try:
+		# Parse the HTML content with BeautifulSoup
+		soup = BeautifulSoup(response.content, "html.parser")
+
+		# Find the specific table by its class name
+		tables = soup.find_all("table", {"class": "pd"})
+		if not tables:
+			print("No tables found with the specified class name.")
+			return None
+
+		table = tables[0]  # Assuming you want the first table
+
+		# Convert the table to a pandas DataFrame
+		df = pd.read_html(StringIO(str(table)))[0]
+		print(df.columns)
+	except Exception as err:
+		print(f"Error parsing the webpage: {err}")
+		return None
+	try:
+		# Clean the DataFrame
+		# Flatten multi-level column headers if they exist, converting non-strings to strings
+		if isinstance(df.columns, pd.MultiIndex):
+			# Flatten the headers, filter out any NaN or None values
+			df.columns = [' '.join(filter(None, map(str, col))) for col in df.columns]
+
+		# Reset the index to clean up the DataFrame
+		df = df.reset_index(drop=True)
+		return df
+		#print(df.iloc[[0],[0, 1, 2]])  # Display the first few rows and columns of the DataFrame
+	except Exception as err:
+		print(f"Error cleaning the DataFrame: {err}")
+		return None
+
+if __name__ == "__main__":
+	k_df = get_tab("https://www.kurzy.cz/podilove-fondy/jtam/jt-opportunity-czk/statistiky/cela-historie/?page=1")
+
+	if k_df is not None:
+		print("Successfully extracted the table!")
+		print(k_df.columns)  # Display the column names
+		# Save the DataFrame to a CSV file with headers.
+		file_name = "jt"
+		k_df.to_csv(f"{file_name}.csv", index=False)
+		print(f"Data saved to '{file_name}.csv'.")
+		print(k_df.iloc[[0,1,2,3],[0, 1, 2]])  # Display the first few rows and columns of the DataFrame
